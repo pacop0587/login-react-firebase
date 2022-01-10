859a72063d86e2e73289176b606e985529032c63
Paco <pacop0587@gmail.com>
2022-01-10 17:21:43 -0600

    Se agrego react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {BrowserRouter, Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 
 function App() {
 
 
   return (
-    <div className="container">
-       
-    </div>
+	  <BrowserRouter>
+		<div className="container-fluid">
+		<Navbar />
+			<Routes>
+				<Route path="login" element='Login'>
+					login
+				</Route>
+				<Route path="admin" element="admin">
+					admin
+				</Route>
+				<Route path='/' element='inicio'>
+
+				</Route>
+			</Routes>
+		</div>
+	</BrowserRouter>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className='navbar navbar-dark bg-dark'>
+            <Link className='navbar-brand'to='/'>AUTH</Link>
+            <div className="d-flex">
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
